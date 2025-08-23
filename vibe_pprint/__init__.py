f008443a7514f0d0848b50b983bb2f8bac3a1c89
akatekhanh <akatekhanh0212@gmail.com>
2025-08-23 18:10:15 +0700

    [release][0.1.1] Vibe pretty print release version 0.1.1 - updating document

--- vibe_pprint/__init__.py
+++ vibe_pprint/__init__.py
@@ -4,7 +4,7 @@ vibe-pretty-print: A smart formatting library for JSON, XML, YAML, and text.
 
 from .core import vibe_pprint
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "Khanh Ngo"
 __email__ = "akatekhanh0212@gmail.com"
 
